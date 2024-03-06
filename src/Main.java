16693f62fa6f2d20acaf86c12005a742a5e4fa29
agorohov <aleksandr.gorohov@gmail.com>
2024-03-06 13:37:52 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int myInt;
+        byte myByte;
+        short myShort;
+        long myLong;
+        float myFloat;
+        double myDouble;
+    }
+}
